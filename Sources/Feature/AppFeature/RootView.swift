45139bba7a8d68d7299d24990fe91cc883d779a8
Narumichi Kubo <narumichi7373@gmail.com>
2023-11-08 15:34:14 +0900

    デバイス座標↔︎画像座標変換とスクロール描画のviewを非表示にする

--- Sources/Feature/AppFeature/RootView.swift
+++ Sources/Feature/AppFeature/RootView.swift
@@ -21,18 +21,18 @@ public struct RootView: View {
     public var body: some View {
         NavigationLayout {
             List {
-                NavigationLink(
-                    mockFiles[0].name,
-                    destination: DrawingView(drawingService: factory.drawingService, file: mockFiles[0])
-                )
+//                NavigationLink(
+//                    mockFiles[0].name,
+//                    destination: DrawingView(drawingService: factory.drawingService, file: mockFiles[0])
+//                )
                 NavigationLink(
                     mockFiles[1].name,
                     destination: DrawingNDCView(drawingService: factory.drawingService, file: mockFiles[1])
                 )
-                NavigationLink(
-                    mockFiles[2].name,
-                    destination: DrawingScroolView(drawingService: factory.drawingService, file: mockFiles[2])
-                )
+//                NavigationLink(
+//                    mockFiles[2].name,
+//                    destination: DrawingScroolView(drawingService: factory.drawingService, file: mockFiles[2])
+//                )
             }
             .navigationTitle("drawing sample app")
             .navigationBarTitleDisplayMode(.inline)
